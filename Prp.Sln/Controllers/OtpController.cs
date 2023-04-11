39b533c94495652073eb21632fd90c1f3a07cf98
Phf-Dev <95465182+Phf-Dev@users.noreply.github.com>
2023-04-11 15:19:29 +0500

    changes for speciality and othr

---
+++ Prp.Sln/Controllers/OtpController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Configuration;
+using Prp.Data;
+
+namespace Prp.Sln.Controllers
+{
+    public class OtpController : Controller
+    {
+        // GET: Otp
+      
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string GetCode(string PhoneNumber)
+        {
+           
+            string code = string.Empty;
+            // set up connection string to your database
+            string connectionString = ConfigurationManager.ConnectionStrings["DbPrpConn"].ConnectionString;
+            SqlConnection con = new SqlConnection(PrpDbConnectADO.Conn);
+            try
+            {
+                string query = "Select top(1) Cast(otpCode as varchar(250)) From tblOtps where mobilenumber = '" + PhoneNumber + "' and isUsed = 0  order by 1 desc";
+                SqlCommand cmdUpdate = new SqlCommand(query);
+                con.Open();
+                cmdUpdate.Connection = con;
+                SqlDataReader reader = cmdUpdate.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    // read the first row and retrieve the code value
+                    reader.Read();
+                     code = reader.GetString(0);
+                    return code;
+                }
+                return code;
+            }
+            catch (Exception ex)
+            {
+               
+            }
+            finally
+            {
+                con.Close();
+            }
+            return code;
+       
+        }
+    }
+}
